aac02a6339be6efc02952183198c062efb57a5c9
huy-hoang.phan <hoanghuy2209@gmail.com>
2018-09-08 14:10:20 +0700

    UI

--- src/components/AddWord.js
+++ src/components/AddWord.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './AddWord.scss';
+
 
 class AddWord extends Component {
   render() {
     return (
       <div className="add-word">
         <div className="new-word">
-          <input className="new-word__input" />
-          <button className="new-word_button">Add</button>
+          <input className="vocab-input new-word__input" />
+          <button className="vocab-button new-word__button">Add</button>
         </div>
+        <a href="/" className="vocab-button random-word">Get a word</a>
       </div>
     );
   }
--- src/components/Word.js
+++ src/components/Word.js
@@ -5,29 +5,32 @@ class Word extends Component {
     return (
       <div className="word">
         <div className="form-element word__name">
-          <label className="form-element__label">Word</label>
-          <input type="text" className="form-element__input" />
+          <label className="vocab-label form-element__label">Word</label>
+          <input type="text" className="vocab-input form-element__input" />
         </div>
-        <div className="word__kind">
-          <label className="form-element__label">Part of speech</label>
-          <input type="text" className="form-element__input" />
+        <div className="form-element word__kind">
+          <label className="vocab-label form-element__label">Part of speech</label>
+          <input type="text" className="vocab-input form-element__input" />
         </div>
-        <div className="word__pronoun">
-          <label className="form-element__label">Pronoun</label>
-          <input type="text" className="form-element__input" />
+        <div className="form-element word__pronoun">
+          <label className="vocab-label form-element__label">Pronoun</label>
+          <input type="text" className="vocab-input form-element__input" />
         </div>
-        <div className="word__mean">
-          <label className="form-element__label">Mean</label>
-          <input type="text" className="form-element__input" />
+        <div className="form-element word__mean">
+          <label className="vocab-label form-element__label">Mean</label>
+          <textarea className="vocab-input vocab-input--multiline form-element__input" />
         </div>
-        <div className="word__example">
-          <label className="form-element__label">Example</label>
-          <input type="text" className="form-element__input" />
+        <div className="form-element word__example">
+          <label className="vocab-label form-element__label">Example</label>
+          <textarea className="vocab-input vocab-input--multiline form-element__input" />
         </div>
         {/* <div className="word__image">
-          <label className="form-element__label">Image</label>
-          <input className="form-element__input" type="file" />
+          <label className="vocab-label form-element__label">Image</label>
+          <input className="vocab-input form-element__input" type="file" />
         </div> */}
+        <div className="form-element form-element--action">
+          <button className="vocab-button">Done</button>
+        </div>
       </div>
     );
   }
