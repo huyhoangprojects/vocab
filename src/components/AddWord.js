f97e9d35d1f618ed1d7e099cc79f0f38d16eae04
huy-hoang.phan <hoanghuy2209@gmail.com>
2018-09-10 00:40:15 +0700

    function add word

--- src/components/AddWord.js
+++ src/components/AddWord.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './AddWord.scss';
-
+import { addWord } from '../firebase/firebase';
 
 class AddWord extends Component {
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+  onClick() {
+    addWord(this.textInput.value)
+  }
   render() {
     return (
       <div className="add-word">
         <div className="new-word">
-          <input className="vocab-input new-word__input" />
-          <button className="vocab-button new-word__button">Add</button>
+          <input className="vocab-input new-word__input" ref={(input) => { this.textInput = input; }}/>
+          <button className="vocab-button new-word__button" onClick={this.onClick}>Add</button>
         </div>
         <Link to="/word/99" className="vocab-button random-word">Get a word</Link>
       </div>
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,3 +8,17 @@ const config = {
   messagingSenderId: "67551096587"
 };
 firebase.initializeApp(config);
+
+export function addWord(word) {
+  firebase.database().ref('spideycode').orderByChild('id').limitToLast(1).on('value', function(data) {
+    data;
+    const key = Object.keys(data.val());
+    debugger;
+    const id = data.val()[key[0]].id;
+    debugger;
+    firebase.database().ref('spideycode/'+word).set({
+      id: id + 1,
+      status: 'new'
+    });
+  });
+}
