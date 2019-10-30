20329707de74e1a8d5de2e5f2671d1d6495c1c3f
spidey <spideycode18@gmail.com>
2019-10-30 22:48:26 +0700

    production

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 import './firebase/firebase';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,42 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
-	module.exports = {
-	  module: {
-	    rules: [
-	      {
-	        test: /\.(js|jsx)$/,
-	        use: ['babel-loader'],
-	        exclude: /node_modules/,
-	      },
-	      {
-	        test: /\.(scss|css)$/,
-	        use: [{
-	          loader: "style-loader" // creates style nodes from JS strings
-	        }, {
-	          loader: "css-loader" // translates CSS into CommonJS
-	        }, {
-	          loader: "sass-loader" // compiles Sass to CSS
-	        }]
-	      },
-	      {
-	        test: /\.(jpe?g|png|gif|svg|ttf|eot|woff|woff2)$/i,
-	        use: [
-	          {
-	            loader: 'file-loader',
-	          },
-	        ]
-	      }
-	    ]
-	  },
-	  plugins: [
-	    new HtmlWebPackPlugin({
-	      template: "./public/index.html",
-	      filename: "./index.html"
-	    })
-	  ],
-	  devServer: {
-	    contentBase: path.join(__dirname, "public"),
-			compress: true,
-			historyApiFallback: true,
-	    port: 8000
-	  }
+	module.exports = (env) =>	 {
+		return {
+			entry: './src/index.js',
+			output: {
+				path: path.join(__dirname, 'public'),
+				filename: 'main.js'
+			},
+			module: {
+				rules: [
+					{
+						test: /\.(js|jsx)$/,
+						use: ['babel-loader'],
+						exclude: /node_modules/,
+					},
+					{
+						test: /\.(scss|css)$/,
+						use: [{
+							loader: "style-loader" // creates style nodes from JS strings
+						}, {
+							loader: "css-loader" // translates CSS into CommonJS
+						}, {
+							loader: "sass-loader" // compiles Sass to CSS
+						}]
+					},
+					{
+						test: /\.(jpe?g|png|gif|svg|ttf|eot|woff|woff2)$/i,
+						use: [
+							{
+								loader: 'file-loader',
+							},
+						]
+					}
+				]
+			},
+			devServer: {
+				contentBase: path.join(__dirname, "public"),
+				historyApiFallback: true,
+			}
+		};
 	};
